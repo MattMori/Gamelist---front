7926985481cab6c64050f65162951ce9ac76a5c2
MattMori <Mateus5310@live.com>
2023-10-12 20:01:31 -0300

    Adição do footer da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Home from "./pages/Home/Home.jsx";
 import Login from "./pages/Login/Login.jsx";
 import './index.scss';
 import GameDetail from "./pages/GameDetail/GameDetail";
- function App() {
+import Footer from "./components/footer/Footer";
+function App() {
   const [searchValue, setSearchValue] = useState("");
 
-  
+
   return (
     <div className="App">
       <Header onSubmit={(inputValue) => setSearchValue(inputValue)} />
-       <Routes>
+      <Routes>
         <Route path="/" element={<Home searchValueProp={searchValue} />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/game/:id" element={<GameDetail />} />
-              </Routes>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
