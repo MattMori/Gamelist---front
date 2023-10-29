0f79d4ab5b2b65d8983608ca555e9a65f6c76914
MattMori <mateus5310@live.com>
2023-10-29 20:39:55 -0300

    Correção no score

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -4,13 +4,7 @@ import { FaRegStar, FaCalendarAlt } from 'react-icons/fa';
 import './index.scss';
 
 const GameCard = ({ game }) => {
-  const formatarDataBrasileira = (data) => {
-    const dataObjeto = new Date(data);
-    const dia = dataObjeto.getDate().toString().padStart(2, '0');
-    const mes = (dataObjeto.getMonth() + 1).toString().padStart(2, '0');
-    const ano = dataObjeto.getFullYear();
-    return `${dia}/${mes}/${ano}`;
-  };
+  
 
   return (
     <Link to={`/game/${game.id}`} className='btn-details'>
@@ -20,7 +14,7 @@ const GameCard = ({ game }) => {
         </div>
         <div className='details-text'>
             <h3>{game.name}</h3>
-            <p><FaRegStar size={15} /> {game.rating}/10
+            <p><FaRegStar size={15} /> {game.rating.toFixed(1)}/5
                  || {(game.genres ? game.genres.map(genre => genre.name).join(', ') : 'N/A')}
             </p>
           </div>
